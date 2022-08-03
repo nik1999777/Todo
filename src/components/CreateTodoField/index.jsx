ef78a973514abe05c548602409dc491a92a639d0
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2022-08-03 17:13:37 +0300

    asasdasdaasd12

--- src/components/CreateTodoField/index.jsx
+++ src/components/CreateTodoField/index.jsx
@@ -2,43 +2,43 @@ import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const CreateTodoField = ({ setTodos }) => {
-	const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('')
 
-	const addTodo = title => {
-		setTodos(prev => [
-			{
-				id: uuidv4(),
-				title,
-				isCompleted: false,
-			},
-			...prev,
-		])
-		setTitle('')
-	}
+  const addTodo = title => {
+    setTodos(prev => [
+      {
+        id: uuidv4(),
+        title,
+        isCompleted: false,
+      },
+      ...prev,
+    ])
+    setTitle('')
+  }
 
-	// asdasd
-	//
+  // asdasd
+  //
 
-	return (
-		<>
-			<input
-				className='flex items-center justify-between mb-4 rounded-2xl border-gray-800 border-2 px-5 py-3 w-full mt-20'
-				type='text'
-				onChange={e => setTitle(e.target.value)}
-				value={title}
-				onKeyPress={e => e.key === 'Enter' && addTodo(title)}
-				placeholder='Add a task'
-			/>
+  return (
+    <>
+      <input
+        className='flex items-center justify-between mb-4 rounded-2xl border-gray-800 border-2 px-5 py-3 w-full mt-20'
+        type='text'
+        onChange={e => setTitle(e.target.value)}
+        value={title}
+        onKeyPress={e => e.key === 'Enter' && addTodo(title)}
+        placeholder='Add a task'
+      />
 
-			<button
-				onChange={e => setTitle(e.target.value)}
-				onClick={() => addTodo(title)}
-				className='bg-gray-800 w-52 h-12 rounded-full  my-0 mx-auto block text-zinc-50'
-			>
-				Add
-			</button>
-		</>
-	)
+      <button
+        onChange={e => setTitle(e.target.value)}
+        onClick={() => addTodo(title)}
+        className='bg-gray-800 w-52 h-12 rounded-full  my-0 mx-auto block text-zinc-50'
+      >
+        Add
+      </button>
+    </>
+  )
 }
 
 export default CreateTodoField
